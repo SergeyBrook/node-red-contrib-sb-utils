df4b473918f9817b8339da46aafd48dfeec38495
Sergey Brook <brook.sergey@gmail.com>
2022-01-20 09:41:15 +0200

    Add router node

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,9 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com) and this project adheres to [Semantic Versioning](https://semver.org).
 
-## [Unreleased]
+## Unreleased
+### Added
+- `sb-router` node.
 ### Changed
 - Add `sb` prefix to nodes and examples file names.
 - Rename `getEvent` method to `getStateEvent` in watchdog and gate nodes for consistency.
---
+++ nodes/sb-router.js
@@ -0,0 +1,82 @@
+module.exports = function(RED) {
+	'use strict';
+
+
+	/**
+	 * SBRouterNode
+	 * SB Router node
+	 * @param {*} config 
+	 */
+	function SBRouterNode(config) {
+		// Init node:
+		RED.nodes.createNode(this, config);
+		let node = this;
+
+		// Node props:
+		this.name = config.name;
+		this.control = config.control;
+
+		// Router:
+		this.router = {
+			output: 1,
+			outputsNum: parseInt(config.outputs),
+			init: function() {
+				this.output = 1;
+				node.status(this.getStatus());
+			},
+			setOutput: function(num) {
+				if (Number.isInteger(num) && num > 0 && num <= this.outputsNum) {
+					this.output = num;
+					node.status(this.getStatus());
+					return true;
+				} else {
+					return false;
+				}
+			},
+			getStatus: function() {
+				return {
+					fill: 'green',
+					shape: 'dot',
+					text: 'Output: ' + this.output
+				};
+			}
+		};
+
+		// On message input event handler:
+		this.on('input', function(msg, send, done) {
+			if (msg.hasOwnProperty(node.control)) {
+				// Received control message:
+				let command = parseInt(msg[node.control]);
+				if (node.router.setOutput(command)) {
+					// Notify done:
+					done();
+				} else {
+					// Invalid command.
+					// Trigger Catch node:
+					done('Invalid command');
+				}
+			} else {
+				// Received regular message:
+				// Prepare output data:
+				let data = new Array(node.router.outputsNum);
+				data[node.router.output - 1] = msg;
+				// Send:
+				send(data);
+				// Notify done:
+				done();
+			}
+		});
+
+		// On node close event handler:
+		this.on('close', function(done) {
+			// Notify done:
+			done();
+		});
+
+		// Init router:
+		node.router.init();
+	}
+
+	// Register node:
+	RED.nodes.registerType('sb-router', SBRouterNode);
+}
